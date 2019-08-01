1416b5526156cc9d1e521c13e7abfa532677bb8c
coolguy-3 <joshuacaruso1@outlook.com>
2019-08-01 08:51:06 -0500

    exercises

--- exercises/small_problems/easy_2.rb
+++ exercises/small_problems/easy_2.rb
@@ -1,2 +1,121 @@
-teddy_age = rand(20..200)
-puts "Teddy is #{teddy_age} years old!"
+#teddy_age = rand(20..200)
+#"Teddy is #{teddy_age} years old!"
+
+#print "Enter the length of the room in meters: "
+#length = gets.chomp.to_f
+#print "Enter the width of the room in meters: "
+#width = gets.chomp.to_f
+
+#square_meters = length * width
+#square_feet = square_meters * 10.7639
+#puts "The area of the room is #{square_meters} (#{square_feet} square feet)."
+
+#print "What is the bill? "
+#bill = gets.chomp.to_f
+#print "What is the tip percentage? "
+#tip_perc = gets.chomp.to_f
+#puts ""
+#tip = bill * (tip_perc / 100)
+#puts "The tip is $#{tip}"
+#puts "The total is $#{bill + tip}"
+
+#print "What is your age? "
+#age = gets.chomp.to_i
+#print "At what age would you like to retire? "
+#retire_age = gets.chomp.to_i
+#puts ""
+#year = Time.now.year
+#how_many = retire_age - age
+#puts "It's #{year}. You will retire in #{year + how_many}."
+#puts "You only have #{how_many} years of work to go!"
+
+#print "What is you name? "
+#name = gets.chomp
+#if name.split("").include? "!"
+#yell_name = name.split("")
+#yell_name.pop
+#yell_name = yell_name.join.upcase
+#puts "HELLO #{yell_name}. WHY ARE WE SCREAMING?"
+#else
+#puts "Hello #{name}."
+#end
+
+#print 'What is your name? '
+#name = gets.chomp
+
+#if name[-1] == '!'
+#    name = name.chop
+#    puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+#    else
+#    puts "Hello #{name}."
+#end
+
+#for n in (1..99)
+#  if n.odd?
+#    puts n
+#  end
+#end
+
+#print "> Please enter an integer greater than 0: "
+#int = nil
+#
+#loop do
+#  int = gets.chomp.to_i
+#  if int.is_a?(Integer) && int > 0
+#    break
+#  else
+#    print "> Please enter a valid Integer greater than 0: "
+#  end
+#end
+#
+#print "> Enter 's' to compute the sum, 'p' to compute the product: "
+#s_or_p = nil
+#
+#loop do
+#    s_or_p = gets.chomp.downcase
+#    if s_or_p == 's' || s_or_p == 'p'
+#        break
+#        else
+#        print "> Please enter s or p: "
+#    end
+#end
+#
+#if s_or_p == 's'
+#  sum = 0
+#  for number in (1..int)
+#    sum += number
+#  end
+#  puts sum
+#else
+#  product = 1
+#  for number in (1..int)
+#    product *= number
+#  end
+#  puts product
+#end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ exercises/small_problems/easy_3.rb
@@ -0,0 +1,124 @@
+#num_array = []
+#for number in (1..5)
+#  print "==> Enter number #{number}: "
+#  num_array << gets.chomp.to_i
+#end
+#
+#print "==> Enter final number: "
+#final_number = gets.chomp.to_i
+#
+#if num_array.include? final_number
+#  puts "The number #{final_number} appears in #{num_array}."
+#else
+#puts "The number #{final_number} does not appear in #{num_array}."
+#end)
+
+#print "Enter the first number: "
+#a = gets.chomp.to_i
+#print "Enter the second number: "
+#b = gets.chomp.to_i
+
+#puts "#{a} + #{b} = #{a + b}"
+#puts "#{a} - #{b} = #{a - b}"
+#puts "#{a} * #{b} = #{a * b}"
+#puts "#{a} / #{b} = #{a / b}"
+#puts "#{a} % #{b} = #{a % b}"
+#puts "#{a} ** #{b} = #{a ** b}"
+
+#print "Please write word or multiple words: "
+#input = gets.chomp
+#puts "There are #{input.split(" ").join("").size} characters in #{input}."
+
+#def multiply(x, y)
+#  x * y
+#end
+#
+#def square(x)
+#  multiply(x, x)
+#end
+
+#def xor?(x, y)
+#  if x && y
+#    false
+#  elsif x == false && y == false
+#    false
+#  else
+#    true
+#  end
+#end
+
+#puts xor?(5.even?, 4.even?)
+#puts xor?(5.odd?, 4.odd?)
+#puts xor?(5.odd?, 4.even?)
+#puts xor?(5.even?, 4.odd?)
+
+
+#def oddities(array)
+#  array_new = []
+#  counter = 0
+#  array.each do |obj|
+#    if counter.odd? == false
+#      array_new << obj
+#   end
+#    counter += 1
+#  end
+#  puts array_new
+#end
+
+#oddities([2, 3, 4, 5, 6])
+#oddities(['abc', 'def'])
+#oddities([123])
+#oddities([])
+
+#def palindrome?(string)
+#    string == string.reverse
+#end
+#
+#def real_palindrome?(string)
+#    string = string.downcase.delete('^a-z0-9')
+#    palindrome?(string)
+#end
+#
+#puts real_palindrome?('madam')
+#puts real_palindrome?('Madam')
+#puts real_palindrome?("Madam, I'm Adam")
+#puts real_palindrome?('356653')
+#puts real_palindrome?('3956a6593')
+#puts real_palindrome?('123ab321')
+
+def palindromic_number?(num)
+  num.to_s == num.to_s.reverse
+end
+
+puts palindromic_number?(34543)
+puts palindromic_number?(123210)
+puts palindromic_number?(22)
+puts palindromic_number?(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
